48256380232d37c28d61c63682a341313bd608f0
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-10-13 13:32:31 -0400

    model of links

---
+++ src/models/links.model.ts
@@ -0,0 +1,6 @@
+export const LinksMenu = [
+  { name: "Home", url: "/", icon: "RiHome7Line" },
+  { name: "Projects", url: "/projects", icon: "RiBriefcaseLine" },
+  { name: "About", url: "/about", icon: "RiUserLine" },
+  { name: "Contact", url: "/contact", icon: "RiContactsLine" },
+];
