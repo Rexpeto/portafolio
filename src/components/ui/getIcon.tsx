aae8e02436b8152ad826c98cd884fc9516d42671
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-10-13 13:28:14 -0400

    dynamic icons

---
+++ src/components/ui/getIcon.tsx
@@ -0,0 +1,22 @@
+import * as Ri from "react-icons/ri";
+
+interface Props {
+  icon: string;
+  className?: string;
+}
+
+const GetIcon = ({ icon, className }: Props) => {
+  const getIcon = (iconName: string) => {
+    const iconsMap = new Map();
+    iconsMap.set("Ri", Ri);
+
+    return iconsMap.get(iconName.substring(0, 2));
+  };
+
+  const icons = getIcon(icon);
+  const TheIcon = icons[icon];
+
+  return <TheIcon className={className} />;
+};
+
+export default GetIcon;
