cde52d1b4927c2b9fb14134eb3c42d189ba9644a
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-10-13 13:32:58 -0400

    index page

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { Sidebar } from "@/components/shared";
+import { GetIcon } from "@/components/ui";
+import { useState } from "react";
+
+export default function MainLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [showMenu, setShowMenu] = useState(false);
+
+  return (
+    <>
+      <Sidebar showMenu={showMenu} onClose={() => setShowMenu(false)} />
+
+      <button
+        className="lg:hidden p-4 m-4 border border-gray-700/40 rounded-md"
+        onClick={() => setShowMenu(true)}
+      >
+        <GetIcon icon="RiMenuLine" className="text-xl" />
+      </button>
+
+      <main className="pl-[16vw] h-full container mx-auto py-5 lg:py-8">
+        {children}
+      </main>
+    </>
+  );
+}
