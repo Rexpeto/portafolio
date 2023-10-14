74e77611afce1e8009f075afc18e0705771ad0c1
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-10-14 15:55:56 -0400

    links socials

--- src/models/links.model.ts
+++ src/models/links.model.ts
@@ -4,3 +4,12 @@ export const LinksMenu = [
   { name: "About", url: "/about", icon: "RiUserLine" },
   { name: "Contact", url: "/contact", icon: "RiContactsLine" },
 ];
+
+export const LinkSocials = [
+  { name: "GitHub", url: "https://github.com/rexpeto", icon: "RiGithubLine" },
+  {
+    name: "LinkedIn",
+    url: "https://www.linkedin.com/in/rexpeto",
+    icon: "RiLinkedinLine",
+  },
+];
