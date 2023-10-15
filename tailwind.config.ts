646959b71df31a98188986633922a5c8de5d4ae7
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-10-15 12:13:14 -0400

    animations slider

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,17 @@ const config: Config = {
         primary: "#37a9c7",
         secondary: "#ccccaa",
       },
+      animation: {
+        ["infinite-slider"]: "infiniteSlider 20s linear infinite",
+      },
+      keyframes: {
+        infiniteSlider: {
+          "0%": { transform: "translateX(0)" },
+          "100%": {
+            transform: "translateX(calc(-250px * 5))",
+          },
+        },
+      },
     },
   },
   plugins: [],
