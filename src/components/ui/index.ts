c1877dde1442388e8add5c4559d1187f3be8cab2
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-10-17 09:29:30 -0400

    update barrel

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -3,3 +3,4 @@ export { default as SocialButton } from "./links-Social";
 export { default as LinkCustom } from "./Link-custom";
 export { default as Title } from "./title";
 export { default as CardWork } from "./card-Work";
+export { default as Button } from "./button";
