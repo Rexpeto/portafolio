79bab1e94f9a98c0e2877b77b28b8c57671baaec
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-10-17 09:32:40 -0400

    project test

---
+++ src/models/projects.model.ts
@@ -0,0 +1,154 @@
+export const Projects = [
+  {
+    id: 1,
+    title: "Project 1",
+    tecnologies: [
+      {
+        name: "Typescript",
+        icon: "SiTypescript",
+        badge: "badges-typescript",
+        color: "color-typescript",
+      },
+      {
+        name: "Next.js",
+        icon: "SiNextdotjs",
+        badge: "badges-next",
+        color: "color-next",
+      },
+      {
+        name: "Redux",
+        icon: "SiRedux",
+        badge: "badges-redux",
+        color: "color-redux",
+      },
+      {
+        name: "Tailwind",
+        icon: "SiTailwindcss",
+        badge: "badges-tailwind",
+        color: "color-tailwind",
+      },
+    ],
+    image:
+      "https://img.freepik.com/vector-gratis/comprar-pantallas-aplicaciones-incorporacion-linea_23-2148407462.jpg?w=740&t=st=1697399712~exp=1697400312~hmac=96e819336a06bbcaae1dd1a9b9c67aed45fb4a72ef7d3a677d0c0086c56efb3b",
+  },
+  {
+    id: 2,
+    title: "Project 2",
+    tecnologies: [
+      {
+        name: "Typescript",
+        icon: "SiTypescript",
+        badge: "badges-typescript",
+        color: "color-typescript",
+      },
+      {
+        name: "Redux",
+        icon: "SiRedux",
+        badge: "badges-redux",
+        color: "color-redux",
+      },
+      {
+        name: "Tailwind",
+        icon: "SiTailwindcss",
+        badge: "badges-tailwind",
+        color: "color-tailwind",
+      },
+      {
+        name: "docker",
+        icon: "SiDocker",
+        badge: "badges-docker",
+        color: "color-docker",
+      },
+      {
+        name: "Sass",
+        icon: "SiSass",
+        badge: "badges-sass",
+        color: "color-sass",
+      },
+      {
+        name: "Remix",
+        icon: "SiRemix",
+        badge: "badges-remix",
+        color: "color-remix",
+      },
+    ],
+    image:
+      "https://img.freepik.com/vector-gratis/comprar-pantallas-aplicaciones-incorporacion-linea_23-2148407462.jpg?w=740&t=st=1697399712~exp=1697400312~hmac=96e819336a06bbcaae1dd1a9b9c67aed45fb4a72ef7d3a677d0c0086c56efb3b",
+  },
+  {
+    id: 1,
+    title: "Project 1",
+    tecnologies: [
+      {
+        name: "Typescript",
+        icon: "SiTypescript",
+        badge: "badges-typescript",
+        color: "color-typescript",
+      },
+      {
+        name: "Next.js",
+        icon: "SiNextdotjs",
+        badge: "badges-next",
+        color: "color-next",
+      },
+      {
+        name: "Redux",
+        icon: "SiRedux",
+        badge: "badges-redux",
+        color: "color-redux",
+      },
+      {
+        name: "Tailwind",
+        icon: "SiTailwindcss",
+        badge: "badges-tailwind",
+        color: "color-tailwind",
+      },
+    ],
+    image:
+      "https://img.freepik.com/vector-gratis/comprar-pantallas-aplicaciones-incorporacion-linea_23-2148407462.jpg?w=740&t=st=1697399712~exp=1697400312~hmac=96e819336a06bbcaae1dd1a9b9c67aed45fb4a72ef7d3a677d0c0086c56efb3b",
+  },
+  {
+    id: 2,
+    title: "Project 2",
+    tecnologies: [
+      {
+        name: "Typescript",
+        icon: "SiTypescript",
+        badge: "badges-typescript",
+        color: "color-typescript",
+      },
+      {
+        name: "Redux",
+        icon: "SiRedux",
+        badge: "badges-redux",
+        color: "color-redux",
+      },
+      {
+        name: "Tailwind",
+        icon: "SiTailwindcss",
+        badge: "badges-tailwind",
+        color: "color-tailwind",
+      },
+      {
+        name: "docker",
+        icon: "SiDocker",
+        badge: "badges-docker",
+        color: "color-docker",
+      },
+      {
+        name: "Sass",
+        icon: "SiSass",
+        badge: "badges-sass",
+        color: "color-sass",
+      },
+      {
+        name: "Remix",
+        icon: "SiRemix",
+        badge: "badges-remix",
+        color: "color-remix",
+      },
+    ],
+    image:
+      "https://img.freepik.com/vector-gratis/comprar-pantallas-aplicaciones-incorporacion-linea_23-2148407462.jpg?w=740&t=st=1697399712~exp=1697400312~hmac=96e819336a06bbcaae1dd1a9b9c67aed45fb4a72ef7d3a677d0c0086c56efb3b",
+  },
+];
