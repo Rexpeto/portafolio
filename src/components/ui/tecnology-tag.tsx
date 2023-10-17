05411e4bfcf4fc861c34a57a422193267924132d
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-10-17 09:31:38 -0400

    badges of tecnology

---
+++ src/components/ui/tecnology-tag.tsx
@@ -0,0 +1,25 @@
+import { GetIcon } from ".";
+
+interface Props {
+  tecnology: Tecnology;
+}
+
+interface Tecnology {
+  name: string;
+  icon: string;
+  badge: string;
+  color: string;
+}
+
+const TecnologyTag = ({ tecnology }: Props) => {
+  return (
+    <span
+      className={`${tecnology.badge} inline-flex gap-2 items-center text-white text-xs font-medium mr-2 mb-2 px-2.5 py-0.5 rounded-md  border`}
+    >
+      <GetIcon icon={tecnology.icon} />
+      {tecnology.name}
+    </span>
+  );
+};
+
+export default TecnologyTag;
