cdb5365a743ccf13b328cedfe1155cfd34a91bce
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-10-18 20:33:37 -0400

    model of creativity

---
+++ src/models/creativity.models.ts
@@ -0,0 +1,29 @@
+export const CreativyData = [
+  {
+    id: 1,
+    title: "Framer Template",
+    url: "/",
+    description:
+      "Creating Framer Template for designing, agent, and content creator",
+    icon: "AiTwotoneThunderbolt",
+    bg: "color-framer",
+  },
+  {
+    id: 2,
+    title: "Tailored design",
+    url: "/",
+    description:
+      "Creation of layout tailored to the project, providing scalable designs",
+    icon: "AiOutlineAntDesign",
+    bg: "color-design",
+  },
+  {
+    id: 3,
+    title: "Framer Template",
+    url: "/",
+    description:
+      "Creating Framer Template for designing, agent, and content creator",
+    icon: "AiOutlineCode",
+    bg: "color-clean",
+  },
+];
