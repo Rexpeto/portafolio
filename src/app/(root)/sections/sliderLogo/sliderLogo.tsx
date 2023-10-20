b404b3db2629f026f61040a2cb27e1cc4284cc14
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-10-20 16:48:47 -0400

    use api strapi

--- src/app/(root)/sections/sliderLogo/sliderLogo.tsx
+++ src/app/(root)/sections/sliderLogo/sliderLogo.tsx
@@ -1,23 +1,39 @@
 import { GetIcon } from "@/components/ui";
-import { Logos } from "@/models";
 
-const SliderLogo = () => {
+async function getData() {
+  const res = await fetch(`${process.env.API_URL}/technologies`, {
+    next: { revalidate: 60 },
+  });
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+const SliderLogo = async () => {
+  const { data } = await getData();
+
   return (
     <div className="slider">
       <div className="slider__content">
-        {Logos.map((logo, index) => (
-          <div className="slide slider__content-logo" key={index}>
+        {data.map((logo: any) => (
+          <div className="slide slider__content-logo" key={logo.id}>
             <GetIcon
-              icon={logo.icon}
-              className={`slider__content-logo-icon ${logo.color}`}
+              icon={logo.attributes.icon}
+              className={`slider__content-logo-icon ${logo.attributes.color}`}
             />
           </div>
         ))}
-        {Logos.map((logo, index) => (
-          <div className="slide slider__content-logo" key={index}>
+        {data.map((logo: any) => (
+          <div className="slide slider__content-logo" key={logo.id}>
             <GetIcon
-              icon={logo.icon}
-              className={`slider__content-logo-icon ${logo.color}`}
+              icon={logo.attributes.icon}
+              className={`slider__content-logo-icon ${logo.attributes.color}`}
             />
           </div>
         ))}
