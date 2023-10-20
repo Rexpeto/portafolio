f9648f89cc9166fcf85dfa01e895ab2b0d6cc45a
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-10-20 16:48:15 -0400

    use api strapi

--- src/app/(root)/sections/featured-work/featured-work.tsx
+++ src/app/(root)/sections/featured-work/featured-work.tsx
@@ -1,12 +1,30 @@
 import { Button, CardWork, Title } from "@/components/ui";
-import { Projects } from "@/models";
-const FeaturedWork = () => {
+
+async function getData() {
+  const res = await fetch(`${process.env.API_URL}/projects?populate=*`, {
+    next: { revalidate: 60 },
+  });
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+const FeaturedWork = async () => {
+  const { data } = await getData();
+
+  console.log(data);
   return (
     <section className="section__container">
       <Title title="Featured Work" />
       <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 mb-5">
-        {Projects.map((project) => (
-          <CardWork key={project.id} project={project} />
+        {data.map((project: any) => (
+          <CardWork key={project.id} project={project.attributes} />
         ))}
       </div>
       <Button href="/projects" label="View More" type="link" />
