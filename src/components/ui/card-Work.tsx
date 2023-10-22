d864f00c59c8622362c942e839c242b138c86a5b
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-10-22 12:14:15 -0400

    import technology-tag

--- src/components/ui/card-Work.tsx
+++ src/components/ui/card-Work.tsx
@@ -1,41 +1,23 @@
 import Image from "next/image";
 import Link from "next/link";
-import TecnologyTag from "./tecnology-tag";
+import TechnologyTag from "./technology-tag";
 
-interface Props {
-  project: Project;
-}
-
-interface Project {
-  id: number;
-  title: string;
-  image: string;
-  tecnologies: Tecnology[];
-}
-
-interface Tecnology {
-  name: string;
-  icon: string;
-  badge: string;
-  color: string;
-}
-
-const CardWork = ({ project }: Props) => {
+const CardWork = ({ project }: { project: any }) => {
   return (
-    <Link href={`/projects/${project.id}`} className="group">
+    <Link href={`/projects/${project.slug}`} className="group">
       <div className="relative w-full h-52 overflow-hidden rounded-lg">
         <Image
-          src={project.image}
-          alt={project.title}
+          src={project.image.data.attributes.url}
+          alt={project?.title}
           className="object-cover group-hover:scale-105 transition duration-150"
           fill
         />
       </div>
       <h3 className="font-semibold text-xl text-white my-4 group-hover:text-primary transition duration-150">
-        {project.title}
+        {project?.title}
       </h3>
-      {project.tecnologies.map((tecnology) => (
-        <TecnologyTag key={tecnology.name} tecnology={tecnology} />
+      {project.technologies?.data.map((technology: any) => (
+        <TechnologyTag key={technology.id} tecnology={technology.attributes} />
       ))}
     </Link>
   );
