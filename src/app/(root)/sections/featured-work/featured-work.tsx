4d42acae3fce3f0e7bf62fa19fe51ba3c521558b
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-10-23 13:58:03 -0400

    use service and model

--- src/app/(root)/sections/featured-work/featured-work.tsx
+++ src/app/(root)/sections/featured-work/featured-work.tsx
@@ -1,31 +1,15 @@
 import { Button, CardWork, Title } from "@/components/ui";
-
-async function getData() {
-  const res = await fetch(
-    `${process.env.API_URL}/projects?pagination[limit]=6&sort[1]=id:desc&locale=en&populate=*`,
-    {
-      next: { revalidate: 60 },
-    },
-  );
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
-
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch data");
-  }
-
-  return res.json();
-}
+import { ProjectData, ProjectResponse } from "@/models";
+import { getProjectsLimit } from "@/services";
 
 const FeaturedWork = async () => {
-  const { data } = await getData();
+  const { data }: ProjectResponse = await getProjectsLimit(6);
 
   return (
     <section className="section__container">
-      <Title title="Featured Work" />
+      <Title title="Featured Projects" />
       <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 mb-5">
-        {data.map((project: any) => (
+        {data.map((project: ProjectData) => (
           <CardWork key={project.id} project={project.attributes} />
         ))}
       </div>
