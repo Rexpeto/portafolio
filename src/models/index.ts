9b92d1eef912716fc17e0902d60c48f19f52d838
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-10-22 20:58:32 -0400

    update barrel

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,3 +2,6 @@ export * from "./links.model";
 export * from "./logo.model";
 export * from "./projects.model";
 export * from "./creativity.models";
+export * from "./hero.model";
+export * from "./general.model";
+export * from "./images.model";
