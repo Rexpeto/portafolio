4cf23ede6b9de585cb0f839feb376c61b367a0ac
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-10-23 14:02:42 -0400

    use model

--- src/components/ui/card-Work.tsx
+++ src/components/ui/card-Work.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
 import Link from "next/link";
 import TechnologyTag from "./technology-tag";
+import { Project, Technologies } from "@/models";
 
-const CardWork = ({ project }: { project: any }) => {
+const CardWork = ({ project }: { project: Project }) => {
   return (
     <Link href={`/projects/${project.slug}`} className="group">
       <div className="relative w-full h-52 overflow-hidden rounded-lg">
@@ -16,7 +17,7 @@ const CardWork = ({ project }: { project: any }) => {
       <h3 className="font-semibold text-xl text-white my-4 group-hover:text-primary transition duration-150">
         {project?.title}
       </h3>
-      {project.technologies?.data.map((technology: any) => (
+      {project.technologies?.data.map((technology: Technologies) => (
         <TechnologyTag key={technology.id} tecnology={technology.attributes} />
       ))}
     </Link>
