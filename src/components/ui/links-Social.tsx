9e10e8ec71967a0103a9a6c42e2a6e4686ced8b3
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-10-23 14:03:38 -0400

    use model

--- src/components/ui/links-Social.tsx
+++ src/components/ui/links-Social.tsx
@@ -1,23 +1,9 @@
 import { RedesResponse } from "@/models";
 import { LinkCustom } from ".";
-
-const getData = async () => {
-  const res = await fetch(`${process.env.API_URL}/redes`, {
-    next: { revalidate: 60 },
-  });
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
-
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch data");
-  }
-
-  return res.json();
-};
+import { getSocials } from "@/services";
 
 const LinksSocial = async () => {
-  const { data }: RedesResponse = await getData();
+  const { data }: RedesResponse = await getSocials();
 
   return (
     <div className="flex gap-2 mt-8">
