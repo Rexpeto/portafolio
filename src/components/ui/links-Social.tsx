223e9ef525f7e0788d701232b296bf97b0314dcf
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-10-24 10:44:02 -0400

    update class

--- src/components/ui/links-Social.tsx
+++ src/components/ui/links-Social.tsx
@@ -6,7 +6,7 @@ const LinksSocial = async () => {
   const { data }: RedesResponse = await getSocials();
 
   return (
-    <div className="flex gap-2 mt-8">
+    <div className="flex items-center gap-2">
       {data.map((link) => (
         <LinkCustom key={link.id} attributes={link.attributes} />
       ))}
