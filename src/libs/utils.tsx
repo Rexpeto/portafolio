b46422609fba4c75f4f46d4f643e0dce5cb56694
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-10-24 13:57:51 -0400

    rich text

--- src/libs/utils.tsx
+++ src/libs/utils.tsx
@@ -1,6 +1,17 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { marked } from "marked";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const richTextReducer = (rowRichText: string) => {
+  const parsedRichText = marked.parse(rowRichText);
+  return (
+    <article
+      className="project-body"
+      dangerouslySetInnerHTML={{ __html: parsedRichText }}
+    />
+  );
+};
