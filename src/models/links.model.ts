eeb30f1652cffe483ea334f72cc37ca19528c5af
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-10-24 13:57:30 -0400

    update

--- src/models/links.model.ts
+++ src/models/links.model.ts
@@ -23,5 +23,9 @@ export const LinksMenu = [
   { name: "Home", url: "/", icon: "RiHome7Line" },
   { name: "Projects", url: "/projects", icon: "RiBriefcaseLine" },
   { name: "About", url: "/about", icon: "RiUserLine" },
-  { name: "Contact", url: "/contact", icon: "RiContactsLine" },
+  {
+    name: "Contact",
+    url: "https://www.linkedin.com/in/rexpeto",
+    icon: "RiContactsLine",
+  },
 ];
