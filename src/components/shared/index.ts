d6a93d9246f1308d1266171dd2131cdff9f1c599
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-10-26 15:59:45 -0400

    update barrel

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -1,3 +1,4 @@
 export { default as Sidebar } from "./sidebar";
 export { default as NavSidebar } from "./navSidebar";
 export { default as TransitionSection } from "./transitionSection";
+export { default as Footer } from "./footer";
